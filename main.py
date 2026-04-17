511972f8e74208791434c9d8fa13c58021153b96
Anna Buckel <abuckel0503@gmail.com>
2026-04-17 14:44:29 +0000

    update

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 from chromadb.utils import embedding_functions
 
 app = FastAPI()
+embedder = embedding_functions.DefaultEmbeddingFunction()
 
 @app.get("/embed")
 def embed(documents: str):
-    embedder = embedding_functions.DefaultEmbeddingFunction()
     vector = embedder(documents)[0].tolist()
     return {"vector": vector}
 
