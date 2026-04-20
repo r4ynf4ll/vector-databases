a20d9e38d68a6cb248b00feaba2cb2da7f05a90e
Anna Buckel <abuckel0503@gmail.com>
2026-04-20 14:41:59 +0000

    troubleshooting similarity

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+# filepath: /workspaces/vector-databases/main.py
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
+from pydantic import BaseModel
 from chromadb.utils import embedding_functions
+import numpy as np
 
 app = FastAPI()
 embedder = embedding_functions.DefaultEmbeddingFunction()
 
 @app.get("/embed")
-def embed(documents: str):
-    vector = embedder(documents)[0].tolist()
+def embed_1(documents: str):
+    vector = embedder([documents])[0].tolist()
     return {"vector": vector}
 
+@app.post("/cosine_similarity")
+def cos_sim(v1:str, v2:str):
+    v1 = embedder([v1])[0]
+    v2 = embedder([v2])[0]
+    cosine_similarity = float(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+    return {"cosine_similarity": cosine_similarity}
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
